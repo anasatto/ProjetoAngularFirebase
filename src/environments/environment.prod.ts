cc98eccc846f40a215f5e5c8affe5ecd240ede10
Ana <129910541+anasatto@users.noreply.github.com>
2025-02-19 19:26:35 +0000

    Configuração do firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyAgqpnXNxZ8abif8ssQq_C2s998w7jZb68",
-    authDomain: "api-aula-d6451.firebaseapp.com",
-    projectId: "api-aula-d6451",
-    storageBucket: "api-aula-d6451.appspot.com",
-    messagingSenderId: "48648388488",
-    appId: "1:48648388488:web:dc73e0a1496a52cd4b843d",
-    measurementId: "G-CR3MDWMWJF"
+  apiKey: "AIzaSyADb-Vsirppx0NDThRln9S4UPNimG7Jxh0",
+  authDomain: "aula-pam2-32cd4.firebaseapp.com",
+  projectId: "aula-pam2-32cd4",
+  storageBucket: "aula-pam2-32cd4.firebasestorage.app",
+  messagingSenderId: "267532859107",
+  appId: "1:267532859107:web:062a7c51d6571f3232f7d6",
+  measurementId: "G-6ED4P2WHTS"
   },
   production: true
 };
